b4a8a5c78bbe4326a8f69b1208e6728dfa053adb
Votre Nom <streaburn@protonmail.com>
2026-06-03 11:30:16 +0200

    Added view template

--- apps/table/views.py
+++ apps/table/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from apps.table import models
+
 
 # Create your views here.
 def home(request):
-    return render(request, "table/home.html")
+    context = {
+        "tables": models.DYT_DynamicTable.objects.all()
+    }
+    print(context)
+    return render(request, "table/home.html", context=context)
+
